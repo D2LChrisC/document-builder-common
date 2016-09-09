0bf719fdb6e671bdd041e1b643a891577b8469b7
Chris Carleton <Chris.Carleton@d2l.com>
2016-09-09 12:52:21 -0400

    Test coverage instrumentation.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,10 @@
+import coveralls from 'gulp-coveralls';
 import eslint from 'gulp-eslint';
 import gulp from 'gulp';
+import istanbul from 'gulp-istanbul';
 import mocha from 'gulp-mocha';
+
+const isparta = require('isparta');
 
 gulp.task('lint', () => {
 	return gulp
@@ -10,8 +14,22 @@ gulp.task('lint', () => {
 		.pipe(eslint.failOnError());
 });
 
-gulp.task('test', ['lint'], () => {
+gulp.task('coverage', () => {
+	return gulp
+		.src(['util/**/*.js'])
+		.pipe(istanbul({ instrumenter: isparta.Instrumenter }))
+		.pipe(istanbul.hookRequire());
+})
+
+gulp.task('test', ['lint', 'coverage'], () => {
 	return gulp
 		.src('tests/**/*.tests.js')
-		.pipe(mocha({ reporter: 'spec' }));
+		.pipe(mocha({ reporter: 'spec' }))
+		.pipe(istanbul.writeReports({ reporters: [ 'lcov' ]}));
+});
+
+gulp.task('report-coverage', ['test'], () => {
+	return gulp
+		.src('coverage/lcov.info')
+		.pipe(coveralls());
 });
