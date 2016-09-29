79fe4c22fc2a6f1044c1324071c832a56741acc5
Igor Skomorokh <Igor.Skomorokh@d2l.com>
2016-09-29 14:05:27 +0300

    Adde Logstash to the pipe

--- dist/util/logger.js
+++ dist/util/logger.js
@@ -9,6 +9,10 @@ var _bunyan = require('bunyan');
 
 var _bunyan2 = _interopRequireDefault(_bunyan);
 
+var _bunyanLogstashTcp = require('bunyan-logstash-tcp');
+
+var _bunyanLogstashTcp2 = _interopRequireDefault(_bunyanLogstashTcp);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function createLogger(name, config) {
@@ -19,6 +23,23 @@ function createLogger(name, config) {
 	logStream.level = config.logLevel;
 
 	var logConfig = { name: name, streams: [logStream] };
+
+	if (config.logstashHost) {
+		var logstashStream = {
+			type: 'raw',
+			stream: _bunyanLogstashTcp2.default.createStream({
+				host: config.logstashHost,
+				port: config.logstashPort,
+
+				// If connection is lost to logstash, attempt to reconnect every 200ms
+				// for 20mins.
+				max_connect_retries: 6000,
+				retry_interval: 200
+			})
+		};
+
+		logConfig.streams.push(logstashStream);
+	}
 
 	if (config.streams) {
 		logConfig.streams = logConfig.streams.concat(config.streams);
--- src/util/logger.js
+++ src/util/logger.js
@@ -1,4 +1,5 @@
 import bunyan from 'bunyan';
+import logstash from 'bunyan-logstash-tcp';
 
 export default function createLogger(name, config) {
 	config = config || { logLevel: 'debug' };
@@ -10,6 +11,23 @@ export default function createLogger(name, config) {
 	logStream.level = config.logLevel;
 
 	const logConfig = { name: name, streams: [logStream] };
+
+	if (config.logstashHost) {
+		const logstashStream = {
+			type: 'raw',
+			stream: logstash.createStream({
+				host: config.logstashHost,
+				port: config.logstashPort,
+
+				// If connection is lost to logstash, attempt to reconnect every 200ms
+				// for 20mins.
+				max_connect_retries: 6000,
+				retry_interval: 200
+			})
+		};
+
+		logConfig.streams.push(logstashStream);
+	}
 
 	if (config.streams) {
 		logConfig.streams = logConfig.streams.concat(config.streams);
