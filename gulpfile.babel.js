ffbb4179f378ce3deea7cbbd65986f60063aed84
Chris Carleton <mrchriscarleton@gmail.com>
2018-07-18 11:18:28 -0400

    Upgraded Gulp and Gulp Mocha to resolve high/critical vulnerabilities.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,30 +6,38 @@ import mocha from 'gulp-mocha';
 
 const isparta = require('isparta');
 
-gulp.task('lint', () => {
+function lint() {
 	return gulp
 		.src(['src/**/*.js', 'tests/**/*.js'])
 		.pipe(eslint())
 		.pipe(eslint.format())
 		.pipe(eslint.failOnError());
-});
+}
 
-gulp.task('coverage', () => {
+function coverage() {
 	return gulp
 		.src(['src/**/*.js'])
 		.pipe(istanbul({ instrumenter: isparta.Instrumenter }))
 		.pipe(istanbul.hookRequire());
-});
+}
 
-gulp.task('test', ['lint', 'coverage'], () => {
+gulp.task('lint', lint);
+
+gulp.task('coverage', coverage);
+
+gulp.task('test', gulp.series('lint', 'coverage', () => {
 	return gulp
 		.src('tests/**/*.tests.js')
-		.pipe(mocha({ reporter: 'spec', timeout: 5000 }))
+		.pipe(mocha({
+			reporter: 'spec',
+			timeout: 5000,
+			compilers: 'js:babel-core/register'
+		}))
 		.pipe(istanbul.writeReports({ reporters: [ 'lcov' ]}));
-});
+}));
 
-gulp.task('report-coverage', ['test'], () => {
+gulp.task('report-coverage', gulp.series('test', () => {
 	return gulp
 		.src('coverage/lcov.info')
 		.pipe(coveralls());
-});
+}));
